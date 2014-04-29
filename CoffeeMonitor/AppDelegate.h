e5089fe4c51882e6be1d601bafcfc4593d92731b
Luke Mueller <luke.jordan.mueller@gmail.com>
2014-04-29 10:14:02 -0500

    Initializes an empty xCode project

---
+++ CoffeeMonitor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoffeeMonitor
+//
+//  Created by Luke Mueller on 4/29/14.
+//  Copyright (c) 2014 pivotal.chicago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoffeeMonitor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoffeeMonitor
+//
+//  Created by Luke Mueller on 4/29/14.
+//  Copyright (c) 2014 pivotal.chicago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
